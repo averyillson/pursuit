a79c730ae357d7e3804bb83f93c6fb9fe838166c
Avery Illson <avery@untld.co>
2018-05-07 19:58:24 -0400

    Switch Terminals:
     - Replaced footer copy
     - Added style to list for about.html

--- footer.php
+++ footer.php
@@ -13,15 +13,7 @@
 
 					<footer id="colophon" class="site-footer">
 						<div class="site-info">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pursuit' ) ); ?>"><?php
-								/* translators: %s: CMS name, i.e. WordPress. */
-								printf( esc_html__( 'Proudly powered by %s', 'pursuit' ), 'WordPress' );
-							?></a>
-							<span class="sep"> | </span>
-							<?php
-								/* translators: 1: Theme name, 2: Theme author. */
-								printf( esc_html__( 'Theme: %1$s by %2$s.', 'pursuit' ), 'pursuit', '<a href="http://www.instagram.com/averyillson">Avery Illson</a>' );
-							?>
+							 &copy; Sarah Espedido, <?php echo date('Y'); ?> | All Rights Reserved.
 						</div><!-- .site-info -->
 					</footer><!-- #colophon -->
 				</div><!-- #content -->
